6332fab8aff062a6ddb5daf1fe62b79734be7b6b
Adam <alspdx@gmail.com>
2018-01-12 16:43:02 -0800

    create function to output message if no results from search

--- src/js/doctor-finder-interface.js
+++ src/js/doctor-finder-interface.js
@@ -11,25 +11,30 @@ $(document).ready(function() {
     foundDoctors.then(function(response) {
       const searchResults = JSON.parse(response);
       console.log(searchResults);
-      $('.output').empty().append(`<h3>They went that-a-way!</h3>`);
-      searchResults.data.map(function(doctor) {
-        const doctorNameOutputHTML = `<div class="doctor-name">${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}</div>`;
-        const acceptsNewPatients = `<span>This doctor is ${(doctor.practices[0].accepts_new_patients ? `` : `not `)}accepting new patients`;
-        const doctorWebsite = (doctor.practices[0].website ? `<span>${doctor.practices[0].website}</span>` : ``);
-        const practiceAddressesHTML =`<div class="doctor-address">
-                                        <span>${doctor.practices[0].name}</span>
-                                        <span>${doctor.practices[0].visit_address.street}</span>
-                                        <span>${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.zip}</span>
-                                        <span>${doctor.practices[0].phones[0].number}</span>
-                                      </div>`;
 
-        $('.output').append(`<div class="doctor-container">
-                              ${doctorNameOutputHTML}
-                              ${practiceAddressesHTML}
-                              ${doctorWebsite}
-                              ${acceptsNewPatients}
-                            </div>`);
-      });
+      if (searchResults.data.length > 0) {
+        $('.output').empty().append(`<h3>They went that-a-way!</h3>`);
+        searchResults.data.map(function(doctor) {
+          const doctorNameOutputHTML = `<div class="doctor-name">${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}</div>`;
+          const acceptsNewPatients = `<span>This doctor is ${(doctor.practices[0].accepts_new_patients ? `` : `not `)}accepting new patients`;
+          const doctorWebsite = (doctor.practices[0].website ? `<span>${doctor.practices[0].website}</span>` : ``);
+          const practiceAddressesHTML =`<div class="doctor-address">
+          <span>${doctor.practices[0].name}</span>
+          <span>${doctor.practices[0].visit_address.street}</span>
+          <span>${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.zip}</span>
+          <span>${doctor.practices[0].phones[0].number}</span>
+          </div>`;
+
+          $('.output').append(`<div class="doctor-container">
+          ${doctorNameOutputHTML}
+          ${practiceAddressesHTML}
+          ${doctorWebsite}
+          ${acceptsNewPatients}
+          </div>`);
+        });
+      } else {
+        $('.output').empty().append(`<span>Your search sucked and did not return any results. Do better next time.</span>`)
+      };
     }, function(error) {
       $('.output').html(`<span>There was an error processing your request: ${error.message}</span>`);
     });
